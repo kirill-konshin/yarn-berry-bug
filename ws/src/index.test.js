7ab032eb88ad3bbb39951511adf8d94d445eb3d8
Kirill Konshin <kirill.konshin@gmail.com>
2020-02-14 15:14:29 -0800

    Linker, tests

--- ws/src/index.test.js
+++ ws/src/index.test.js
@@ -1,1 +1,11 @@
-const {hashRouter} = require('react-router');
+//@see https://github.com/yarnpkg/berry/issues/943
+
+it('works with v3', () => {
+    const {Link} = require('react-router');
+    expect(Link).toBeTruthy();
+});
+
+it('works with v5', () => {
+    const {Link} = require('react-router-dom');
+    expect(Link).toBeTruthy();
+});
